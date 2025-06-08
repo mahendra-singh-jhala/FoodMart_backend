2ca9822e1f2b58c0f811096c8ac68e108c7f1384
mahendra-singh-jhala <mahendrasinghjhala444@gmail.com>
2025-06-08 17:04:15 +0530

    create get bakery controller

--- controllers/bakeryController.js
+++ controllers/bakeryController.js
@@ -31,5 +31,18 @@ exports.createBakery = async (res, req) => {
 }
 
 exports.getBakery = async (res, req) => {
-    
+    try {
+        const bakery = await Bakery.find()
+
+        res.status(200).json({
+            message: "Successfully get bakery",
+            bakery
+        })
+        
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal Server error",
+            error: error.message
+        })
+    }
 }
